dcf919316dbc2feda097bb8c38be919a20c7a648
Brody McFarland <brodyamcfarland@gmail.com>
2023-06-15 14:59:37 -0700

    Checking mobile header bug issue

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -3,10 +3,10 @@ const Header = () => {
           <header className="flex items-center justify-center text-xl uppercase tracking-[.5rem] py-5 relative w-full">
                <div className="text-white relative w-full text-center">
                     <div className="flex gap-3 items-center justify-center z-10">
-                         <span className="border-2 border-b-transparent border-r-transparent border-white/40 pl-2">
+                         <span className="text-white border-2 border-b-transparent border-r-transparent border-white/40 pl-2">
                               Task
                          </span>
-                         <span className="border-2 border-t-transparent border-l-transparent border-white/40">
+                         <span className="text-white border-2 border-t-transparent border-l-transparent border-white/40">
                               Flow
                          </span>
                     </div>
